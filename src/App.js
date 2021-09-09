4162c341b2f61dccfd931dd75d40c306e6c7272b
Hemanth kumar <hemanth140896@gmail.com>
2021-09-09 16:12:34 +0530

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.css';
 import "../src/components/Navbar"
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
+import Header from './components/Header'
 
 function App() {
   return (
-    <Navbar/>
+    <>
+    <Particles 
+    className="particles-canvas"
+    params={{
+      Particles:{ 
+        Number: {
+          value:30,
+          density:{
+            enable:true,
+            value_area:900
+          }
+        },
+        shape:{
+          type:"circle",
+          stroke:{
+            width:6,
+            color:"#f9ab00"
+          }
+        }
+      }
+    }}
+    />
+    <Navbar />
+    <Header />
+    </>
   );
 }
 
