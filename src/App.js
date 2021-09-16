76088226f7cac0e932b519526ba2fd60675c8cd1
G <hemanth-kumar.g@atos.net>
2021-09-16 09:55:35 +0530

    About me completed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "../src/components/Navbar"
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from 'react-particles-js';
 import Navbar from './components/Navbar';
-import Header from './components/Header'
+import Header from './components/Header';
+import AboutMe from './components/AboutMe';
 
 function App() {
   return (
@@ -31,6 +32,7 @@ function App() {
     />
     <Navbar />
     <Header />
+    <AboutMe />
     </>
   );
 }
